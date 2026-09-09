abfeb5603569ce04cf367e6d5c8e4b52a39912f2
cout  "Yasin Horani <70699122+Yasin-Horani@users.noreply.github.com>
2026-09-09 15:45:46 +0200

    Init product catalogus project

---
+++ src/main/java/com/yasin/productcatalogus/ProductcatalogusApplication.java
@@ -0,0 +1,13 @@
+package com.yasin.productcatalogus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductcatalogusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProductcatalogusApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yasin/productcatalogus/ProductcatalogusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yasin.productcatalogus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductcatalogusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
